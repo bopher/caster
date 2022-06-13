4425b1235b355374268e29fa7e0c3814da9b40fe
M Ekramy <m.ajamekramy@gmail.com>
2022-06-13 19:52:53 +0430

    add assert helpers

--- main.go
+++ main.go
@@ -11,3 +11,17 @@ func NewCaster(data any) Caster {
 func IsNilErr(err error) bool {
 	return isErrorOf("Caster.Nil", err)
 }
+
+// Assert try to assert value to type T
+func Assert[T any](v any) (T, bool) {
+	_v, ok := v.(T)
+	return _v, ok
+}
+
+// AssertSafe try to assert value to type T or return fallback
+func AssertSafe[T any](v any, fallback T) T {
+	if _v, ok := v.(T); ok {
+		return _v
+	}
+	return fallback
+}
